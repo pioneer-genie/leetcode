a90b69dfdd9053bf55d2e1139d41fc9a12347932
pioneer-genie <80259880+pioneer-genie@users.noreply.github.com>
2022-05-11 19:43:28 +0900

    Time: 138 ms (49.86%), Space: 15.7 MB (85.90%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -2,18 +2,15 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         lx = len(grid[0])
         ly = len(grid)
-        grid_s = [[987654321 for _ in range(lx)] for j in range(ly)]
-        grid_s[0][0] = grid[0][0]
         
-        def dfs(x, y):
-            if (y < ly-1 and grid_s[y][x] + grid[y+1][x] < grid_s[y+1][x]):
-                grid_s[y+1][x] = grid_s[y][x] + grid[y+1][x]
-                dfs(x, y+1)
-            if (x < lx-1 and grid_s[y][x] + grid[y][x+1] < grid_s[y][x+1]):
-                grid_s[y][x+1] = grid_s[y][x] + grid[y][x+1]
-                dfs(x+1, y)
+        for y in range(1, ly):
+            grid[y][0] += grid[y-1][0]
+        for x in range(1, lx):
+            grid[0][x] += grid[0][x-1]
         
-        dfs(0, 0)
-        
-        return grid_s[ly-1][lx-1]
+        for y in range(1, ly):
+            for x in range(1, lx):
+                grid[y][x] += min(grid[y][x-1], grid[y-1][x])
+                
+        return grid[ly-1][lx-1]
             
